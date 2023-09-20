d8a663bd8045bd19e8cc01f70369919c1d251efe
BuiDoKhoiNguyen <khoinguyen05122004@gmail.com>
2023-09-20 08:02:37 +0700

    add threesum with O(N^2)

--- week3/ThreeSum.java
+++ week3/ThreeSum.java
@@ -6,28 +6,16 @@ public class ThreeSum {
 
     private ThreeSum() { }
 
-    public static void printAll(int[] a) {
-        int n = a.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                for (int k = j+1; k < n; k++) {
-                    if (a[i] + a[j] + a[k] == 0) {
-                        StdOut.println(a[i] + " " + a[j] + " " + a[k]);
-                    }
-                }
-            }
-        }
-    }
-
-
-    public static int count(int[] a) {
+    public static int printAll(int[] a) {
         int n = a.length;
         int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i+1; j < n; j++) {
                 for (int k = j+1; k < n; k++) {
                     if (a[i] + a[j] + a[k] == 0) {
+                        StdOut.println(a[i] + " " + a[j] + " " + a[k]);
                         count++;
+
                     }
                 }
             }
@@ -36,11 +24,11 @@ public class ThreeSum {
     }
 
     public static void main(String[] args)  {
-        In in = new In("C:\\Users\\nguye\\IdeaProjects\\algs4\\algs4-data\\2Kints.txt");
+        In in = new In("C:\\Users\\nguye\\IdeaProjects\\algs4\\algs4-data\\1Kints.txt");
         int[] a = in.readAllInts();
 
         Stopwatch timer = new Stopwatch();
-        int count = count(a);
+        int count = printAll(a);
         StdOut.println("elapsed time = " + timer.elapsedTime());
         StdOut.println(count);
     }
--- week3/ThreeSumFastest.java
+++ week3/ThreeSumFastest.java
@@ -3,32 +3,46 @@ package week3;
 import edu.princeton.cs.algs4.*;
 import java.util.Arrays;
 
-public class ThreeSumFast {
-
+public class ThreeSumFastest {
     public static int printAll(int[] a) {
         int n = a.length;
         Arrays.sort(a);
         int count = 0;
         //if (containsDuplicates(a)) throw new IllegalArgumentException("array contains duplicate integers");
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                int k = Arrays.binarySearch(a, -(a[i] + a[j]));
-                if (k > j){
-                    StdOut.println(a[i] + " " + a[j] + " " + a[k]);
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+            int x = a[i];
+            while (left < right) {
+                int sum = x + a[left] + a[right];
+                if (sum == 0) {
+                    StdOut.println(x + " " + a[left] + " " + a[right]);
                     count++;
-                } 
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
             }
         }
         return count;
     }
 
 
+
+    private static boolean containsDuplicates(int[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (a[i] == a[i-1]) return true;
+        return false;
+    }
+
     public static void main(String []args) {
         In in = new In("C:\\Users\\nguye\\IdeaProjects\\algs4\\algs4-data\\1Kints.txt");
         int[] a = in.readAllInts();
         Stopwatch timer = new Stopwatch();
         int count = printAll(a);
-    
         StdOut.println("elapsed time = " + timer.elapsedTime());
         StdOut.println(count);
     }
