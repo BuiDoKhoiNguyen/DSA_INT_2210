43a3c8f8c4974cd46b90a666e6a5c8c9d03f2379
BuiDoKhoiNguyen <khoinguyen05122004@gmail.com>
2023-12-12 23:19:28 +0700

    nothing 2

--- week3/ThreeSumN2/ThreeSumN2.java
+++ week3/ThreeSumN2/ThreeSumN2.java
@@ -1,13 +1,19 @@
-package week3;
-
 import edu.princeton.cs.algs4.*;
 import java.util.Arrays;
 
 public class ThreeSumN2 {
+    private ThreeSumN2() { }
+
+    private static boolean containsDuplicates(int[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (a[i] == a[i-1]) return true;
+        return false;
+    }
 
     public static void printAll(int[] a) {
         int n = a.length;
         Arrays.sort(a);
+        if (containsDuplicates(a)) throw new IllegalArgumentException("array contains duplicate integers");
         for (int i = 0; i < n - 2; i++) {
             int low = i + 1, high = n - 1;
             while (low < high) {
